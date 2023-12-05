f0dbbe882dbf509c17fadf97f3dda201f643c387
sleepy-droid <sebastian.sanchez1@utp.ac.pa>
2023-12-05 15:20:19 -0500

    cambios al footer php y css

--- app/view/layouts/footer.php
+++ app/view/layouts/footer.php
@@ -3,13 +3,15 @@
     <link rel="stylesheet" href="/sancocho/public/assets/css/footer.css">
 </head>
 
-<footer>
+<footer class="site-footer">
     <div class="footer-content">
         <div class="footer-section about">
             <h3>Universidad Tecnológica de Panamá</h3>
-            <p>Dirección: Avenida Universidad Tecnológica de Panamá, Vía Puente Centenario, Campus Metropolitano Víctor Levi Sasso.</p>
-            <p>Teléfono: +507 560-3000</p>
-            <p><a href="mailto:contacto@utp.ac.pa">contacto@utp.ac.pa</a></p>
+            <address>
+                Avenida Universidad Tecnológica de Panamá, Vía Puente Centenario, Campus Metropolitano Víctor Levi Sasso.<br>
+                Teléfono: +507 560-3000<br>
+                <a href="mailto:contacto@utp.ac.pa">contacto@utp.ac.pa</a>
+            </address>
         </div>
         <div class="footer-section links">
             <h3>Mapa del sitio</h3>
@@ -23,14 +25,16 @@
         <div class="footer-section map">
             <h3>Ubicación</h3>
             <!-- Aquí insertarías un mapa embebido, por ejemplo, un iframe de Google Maps -->
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.953287730552!2d-79.53303458474331!3d9.00237929356422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa5fa1a7b42e5ad%3A0x3f9f9fd8e20b8df!2sUniversidad%20Tecnol%C3%B3gica%20de%20Panam%C3%A1!5e0!3m2!1ses!2spa!4v1646149065556!5m2!1ses!2spa"
-                width="400"
-                height="300"
-                style="border:0;"
-                allowfullscreen=""
-                loading="lazy">
-            </iframe>
+            <div class="map-container">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.953287730552!2d-79.53303458474331!3d9.00237929356422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa5fa1a7b42e5ad%3A0x3f9f9fd8e20b8df!2sUniversidad%20Tecnol%C3%B3gica%20de%20Panam%C3%A1!5e0!3m2!1ses!2spa!4v1646149065556!5m2!1ses!2spa"
+                    width="400"
+                    height="300"
+                    style="border:0;"
+                    allowfullscreen=""
+                    loading="lazy">
+                </iframe>
+            </div>
         </div>
     </div>
     <div class="footer-bottom">
